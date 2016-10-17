790384b79a28c364c705767fd3157a4293771fc1
Gabriel Charriez <gabrielc0464@hstat.org>
2016-10-17 20:11:30 +0000

    hw done

---
+++ fizzbuzz.rb
@@ -0,0 +1,12 @@
+def fizzbuzz(n)
+         if n % 5 == 0 && n % 3 == 0
+        puts "FizzBuzz"
+      elsif n % 5 == 0
+        puts "Buzz"
+      elsif n % 3 == 0
+        puts "Fizz"
+      else
+        puts n
+      end
+    end
+fizzbuzz(15)
