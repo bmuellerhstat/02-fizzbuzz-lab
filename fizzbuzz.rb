39f4f0b1a2674712f4d92de984109eab155f5d08
kevinorta <kevino6532@hstat.org>
2016-10-18 15:48:38 +0000

    done

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,6 @@
 def fizzbuzz(num)
-    if num % 5==0 && num % 3==0 "FizzBuzz" 
+    if num % 5==0 && num % 3==0
+        "FizzBuzz" 
         elsif num % 3==0 
             "Fizz"
         elsif num % 5==0 
