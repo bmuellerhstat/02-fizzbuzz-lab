5a66081a0ee263b443dd7a2092e8dda54b2aefd8
rawfurrahman1423 <rawfurr1423@hstat.org>
2016-10-18 20:41:34 +0000

    finished

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+
+
+def number (number)
+   if number%3==0&number%5==0
+        return "FizzBuzz"
+    elsif  number%3==0
+       return "Fizz"
+    else number%5==0
+        return "Buzz"
+   end
+end
+
+
+        
+       
