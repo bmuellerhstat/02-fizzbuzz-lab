dc17a3ba687c57eb24b89a6b6252f48246a50bef
giovannim1166 <giovannim1166@hstat.org>
2016-10-18 02:42:27 +0000

    final 1

---
+++ fizzbuzz.rb
@@ -0,0 +1,11 @@
+def fizzbuz(num)
+   if (num % 3 == 0 && num % 5 == 0)
+        puts "fizzbuzz"
+    elsif (num % 5 == 0)
+    puts "buzz"
+    elsif (num % 3 == 0)
+    puts "fizz"
+else
+    puts "nil"
+end
+end
