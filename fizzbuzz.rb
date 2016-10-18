c7fef159731b06e377bb2efed58467b19da6b756
justing5321 <justing5321@hstat.org>
2016-10-18 18:35:02 +0000

    fix hwk

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,9 @@
 def fizzbuzz(name)
-    if name / 3
-      "Fizz"
-  elsif  name / 5
-      "Buzz"
+    if name % 3 == 0 && name % 5 ==0
+      "FizzBuzz"
+  elsif name % 3 == 0
+    "Fizz"
+  elsif name % 5 == 0
+    "Buzz"
     end
 end
