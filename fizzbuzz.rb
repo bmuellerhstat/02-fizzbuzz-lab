e2560a9f1739ce54c56630025a576317ff421095
Ahmed Abdalla <ahmeda1955@hstat.org>
2016-10-18 17:03:56 +0000

    HW

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,6 +3,13 @@ if number % 5 == 0 && number % 3 == 0
 
 return "FizzBuzz"
 
+elsif number % 3 == 0
+"Fizz"
+
+elsif number % 5 == 0
+"Buzz"
+
+elsif number % 5 && number % 3 == 0
 
 return "FizzBuzz"
 end
