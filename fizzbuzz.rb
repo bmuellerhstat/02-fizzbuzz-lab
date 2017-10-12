dd493faf34e1ebd739e3f246f4390ea9bb9222c7
cheyennen0503 <cheyennen0503@hstat.org>
2017-10-12 17:48:28 +0000

    done

---
+++ fizzbuzz.rb
@@ -0,0 +1,16 @@
+# puts "Number:"
+# number = gets.chomp.to_i
+
+def fizzbuzz(number)
+    if number%5 == 0 && number%3 == 0
+        return "FizzBuzz"
+    elsif number%5 == 0
+        return "Buzz"
+    elsif number%3 == 0
+        return "Fizz"
+    
+    else 
+        return 
+    end 
+        
+end 
