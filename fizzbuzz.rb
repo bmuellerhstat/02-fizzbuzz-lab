095e4d68b645cddcc2fbf9e0f70123ffaae2a7af
Amr Almaz <amra0760@hstat.org>
2017-10-13 01:22:54 +0000

    completo

---
+++ fizzbuzz.rb
@@ -0,0 +1,16 @@
+def fizzbuzz(num)
+    if num%5 == 0 && num%3 == 0
+        return "FizzBuzz"
+    elsif num%3 == 0
+        return "Fizz"
+    elsif num%5 == 0
+        return "Buzz"
+    else
+        return nil
+    end
+end
+
+puts "Please enter a number: "
+user_number = gets.chomp.to_i
+
+fizzbuzz(user_number)
