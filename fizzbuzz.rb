967d498f5449b6a7cf1cde0f935f9c9b5654a39e
davidx4697 <davidx4697@hstat.org>
2017-10-13 00:29:14 +0000

    done

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,11 @@
-def fizz(user_input)
-    if user_input % 3 == 0 && user_input % 5 == 0
-        return "fizzbuzz"
-    elsif user_input % 3 == 0
-        return "fizz"
-    elsif user_input % 5 == 0
-        return "buzz"
+def fizzbuzz(user_input)
+   if user_input % 3 == 0 && user_input % 5 == 0
+        return "FizzBuzz"
+    elsif user_input%3 == 0
+        return "Fizz"
+    elsif user_input%5 == 0
+        return "Buzz"
     else
-        return "nil "
+        return nil
     end
 end
-user_input = "Give me a number"
-fizz(user_input)
